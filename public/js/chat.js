9e6a64516ad820b2f17e22ed69a1ecb700cc08cc
Olysar1 <giorgibarbakadz1@gmail.com>
2023-10-11 18:13:55 +0400

    add basic messaging and location-sharing

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,67 @@
-io();
+const socket = io();
+
+// Elements
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+const $sendLocationButton = document.querySelector("#send-location");
+const $messages = document.querySelector("#messages");
+
+//Templates
+const messageTemplate = document.querySelector("#message-template").innerHTML;
+const locationTemplate = document.querySelector("#location-template").innerHTML;
+
+socket.on("message", (message) => {
+  console.log(message);
+  const html = Mustache.render(messageTemplate, {
+    message,
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
+});
+
+socket.on("locationMessage", (url) => {
+  console.log(url);
+  const html = Mustache.render(locationTemplate, {
+    url,
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
+});
+
+//send messages
+$messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //disable button
+  $messageFormButton.setAttribute("disabled", "disabled");
+
+  const message = e.target.elements.message.value;
+
+  socket.emit("sendMessage", message, (error) => {
+    $messageFormButton.removeAttribute("disabled"); //re-enable button
+    $messageFormInput.value = ""; //clear input field
+    $messageFormInput.focus(); //focus the input field
+
+    if (error) {
+      return console.log(error);
+    }
+
+    console.log("Message delivered!");
+  });
+});
+
+//send location
+$sendLocationButton.addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    return alert("Geolocation is not supported by your browser");
+  }
+
+  $sendLocationButton.setAttribute("disabled", "disabled");
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    const { latitude, longitude } = position.coords;
+
+    socket.emit("sendLocation", { latitude, longitude }, () => {
+      $sendLocationButton.removeAttribute("disabled");
+      console.log("Location delivered!");
+    });
+  });
+});
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
+const Filter = require("bad-words");
 
 const app = express();
 const server = http.createServer(app);
@@ -12,8 +13,34 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-io.on("connection", () => {
-  console.log("new websocket connection established");
+io.on("connection", (socket) => {
+  console.log("New WebSocket connection.");
+
+  socket.emit("message", "Welcome!");
+  socket.broadcast.emit("message", "A new user has joined!");
+
+  socket.on("sendMessage", (message, callback) => {
+    const filter = new Filter();
+
+    if (filter.isProfane(message)) {
+      return callback("Profanity is not allowed.");
+    }
+
+    io.emit("message", message);
+    callback();
+  });
+
+  socket.on("sendLocation", (location, callback) => {
+    io.emit(
+      "locationMessage",
+      `https://www.google.com/maps?q=${location.latitude},${location.longitude}`
+    );
+    callback();
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("message", "a user has left.");
+  });
 });
 
 server.listen(port, () => {
